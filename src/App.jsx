7ceb4c6185fa4eef4855de126ecd061fd972d819
luigitarallo <tarallo.gigi@gmail.com>
2024-11-05 15:25:39 +0100

    use UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Header from "./components/Header.jsx";
-
+import UserInput from "./components/UserInput.jsx";
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  );
 }
 
 export default App;
