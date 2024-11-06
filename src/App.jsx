26cd2fd4be41350253b091aab26be480c64a89d9
luigitarallo <tarallo.gigi@gmail.com>
2024-11-06 11:32:17 +0100

    add use state and props to UserInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    intialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInputValue={userInput} onChangeInput={handleChange} />
     </>
   );
 }
