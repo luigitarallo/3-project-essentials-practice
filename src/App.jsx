0ca0bb0cff240898fa46d8f128b2449b52c28ee4
luigitarallo <tarallo.gigi@gmail.com>
2024-11-06 11:38:48 +0100

    import Results and add input props

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
+import Results from "./components/Results.jsx";
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -23,6 +24,7 @@ function App() {
     <>
       <Header />
       <UserInput userInputValue={userInput} onChangeInput={handleChange} />
+      <Results input={userInput} />
     </>
   );
 }
